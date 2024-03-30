46683c6c3565a56beb44d80dfb0cd128d3e05f91
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2024-03-30 19:01:29 -0300

    Adjusting in create faker users.

--- demo/src/main/kotlin/faker/InitFaker.kt
+++ demo/src/main/kotlin/faker/InitFaker.kt
@@ -3,26 +3,38 @@ package faker
 import io.quarkus.runtime.Startup
 import jakarta.annotation.PostConstruct
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+import repositories.UserRepository
+import services.UserService
 import java.util.*
 
 @Startup
 @ApplicationScoped
 class InitFaker {
 
+    @Inject
+    lateinit var userService: UserService;
+
+    @Inject
+    lateinit var userRepository: UserRepository;
+
     @PostConstruct
     fun init() {
         System.out.println("The default of creation data is 30");
 
 //        var numObjects = readNum("Users");
-
-        UserDataFake().createUsersObj(40);
+        if (userRepository.findAll().list().size <= 500) {
+            UserDataFake(userService).createUsersObj(40);
+        } else {
+            println("Database is getting very full");
+        }
     }
 
 
-    fun readNum(nameObj : String) : Int {
+    fun readNum(nameObj: String): Int {
         val scanner = Scanner(System.`in`);
 
-        val number : Int;
+        val number: Int;
 
         try {
 
@@ -31,7 +43,7 @@ class InitFaker {
 
             scanner.close();
 
-        } catch (e : Exception) {
+        } catch (e: Exception) {
             return 1;
         }
 
--- demo/src/main/kotlin/faker/UserDataFake.kt
+++ demo/src/main/kotlin/faker/UserDataFake.kt
@@ -2,37 +2,48 @@ package faker
 
 import com.github.javafaker.Faker
 import entities.User
+import io.quarkus.runtime.Startup
+import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
+import jakarta.persistence.EntityManager
+import jakarta.persistence.EntityManagerFactory
+import jakarta.persistence.Persistence
 import services.UserService
-import java.util.Scanner
 
-class UserDataFake {
+@Startup
+@ApplicationScoped
+class UserDataFake @Inject constructor(var userService: UserService) {
 
-    @Inject
-    lateinit var userService : UserService;
+    fun createUsersObj(numObjs: Int) {
 
-
-    fun createUsersObj(numObjs : Int) {
-
-        var numDefault : Int = 30;
+        var numDefault: Int = 30
 
         if (numObjs < 1000 || numObjs > 1) {
-            numDefault = numObjs;
+            numDefault = numObjs
         }
 
-        val faker = Faker();
+        val faker = Faker()
 
-        for (i in 1.. numDefault) {
+        for (i in 1..numDefault) {
 
-            var user = User();
+            var user = User()
 
-            user.userName = faker.name().username();
-            user.email = faker.internet().emailAddress();
-            user.name = faker.name().name();
+            user.userName = faker.name().username()
+            user.email = faker.internet().emailAddress()
+            user.name = faker.name().name()
 
-            userService.createUser(user);
+            userService.createUser(user)
 
         }
+
+    }
+
+    fun createManager(): EntityManager {
+        val entityManagerFactory: EntityManagerFactory = Persistence.createEntityManagerFactory("MyPersistenceUnit")
+
+        val manager = entityManagerFactory.createEntityManager()
+
+        return manager
     }
 
 }
