53e82673eff90fb6738ca75bcd8cb30358234663
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2024-03-30 21:25:40 -0300

    Create CorsFilter Handler.

--- demo/src/main/kotlin/helpers/CorsFilter.kt
+++ demo/src/main/kotlin/helpers/CorsFilter.kt
@@ -3,15 +3,21 @@ package helpers
 import jakarta.ws.rs.container.ContainerRequestContext
 import jakarta.ws.rs.container.ContainerResponseContext
 import jakarta.ws.rs.container.ContainerResponseFilter
-import jakarta.ws.rs.core.MultivaluedMap
 import jakarta.ws.rs.ext.Provider
 
 @Provider
 class CorsFilter : ContainerResponseFilter {
-    override fun filter(p0: ContainerRequestContext?, responseContext: ContainerResponseContext?) {
-        val responseHeaders: MultivaluedMap<String, Any> = responseContext!!.headers
+    override fun filter(requestContext: ContainerRequestContext, responseContext: ContainerResponseContext) {
 
-        responseHeaders.add("Access-Control-Allow-Origin", "*")
+
+        val requestHeaders = requestContext.headers;
+        val responseHeaders = responseContext!!.headers
+
+        val origin = requestHeaders.getFirst("Origin")
+
+        var front = "http://localhost:4200";
+
+        responseHeaders.add("Access-Control-Allow-Origin", front);
         responseHeaders.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
         responseHeaders.add("Access-Control-Allow-Credentials", "true")
         responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
